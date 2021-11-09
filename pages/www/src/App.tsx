50683487def9d0b3dc935515f038f30e8b97a00a
xryuseix <ryusei143.shootingstar@gmail.com>
2021-11-10 06:48:28 +0900

    feat: リアルタイムで計算する機能追加

--- pages/www/src/App.tsx
+++ pages/www/src/App.tsx
@@ -2,14 +2,55 @@
 
 import React from "react";
 import * as wasm from "calculator";
+import "./App.css"
+
+type Props = {
+  title: string;
+};
+
+interface State {
+  formula: string;
+  ans: string;
+  equal: string;
+}
+
+class NameForm extends React.Component<{}, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { formula: "( ( 1 + 3 ) * 2 ) * 3", ans: "24", equal: "=" };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ formula: event.target.value });
+    this.setState({ equal: event.target.value.length > 0 ? "=" : "" });
+    this.setState({ ans: wasm.calc(event.target.value) });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <input
+            type="text"
+            value={this.state.formula}
+            onChange={this.handleChange}
+          />
+        </div>
+        <h2>
+          {this.state.formula} {this.state.equal} {this.state.ans}
+        </h2>
+      </div>
+    );
+  }
+}
 
 const App = () => {
-  let formula = "( ( 1 + 3 ) * 2 ) * 3";
-  let ans = wasm.calc(formula);
   return (
     <div>
       <h1>Calculator</h1>
-      <h2>{formula} = {ans}</h2>
+      <NameForm />
     </div>
   );
 };
--- pages/www/webpack.config.js
+++ pages/www/webpack.config.js
@@ -1,31 +1,35 @@
-const path = require('path');
+/** @format */
+
+const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "src/bootstrap.tsx"),
   output: {
     path: path.resolve(__dirname, "public"),
-    filename: "bootstrap.js"
+    filename: "bootstrap.js",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "ts-loader"
+        loader: "ts-loader",
       },
-    ]
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: [".ts", ".tsx", ".js"],
   },
   devServer: {
-    contentBase: path.resolve(__dirname, "public")
+    contentBase: path.resolve(__dirname, "public"),
   },
-  plugins: [
-    new htmlWebpackPlugin({ template: './src/index.html' }),
-  ],
+  plugins: [new htmlWebpackPlugin({ template: "./src/index.html" })],
   experiments: {
     asyncWebAssembly: true,
-    syncWebAssembly: true
-  }
+    syncWebAssembly: true,
+  },
 };
