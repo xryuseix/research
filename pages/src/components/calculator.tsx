45aedb8a492266aeb4b02def60a1d994c60fae32
xryuseix <ryusei143.shootingstar@gmail.com>
2021-11-12 05:16:27 +0900

    feat: 数値，演算子ボタンを追加

--- pages/src/components/calculator.tsx
+++ pages/src/components/calculator.tsx
@@ -14,29 +14,66 @@ interface State {
   equal: string;
 }
 
-class NameForm extends React.Component<{}, State> {
+class CalcForm extends React.Component<{}, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { formula: "( ( 1 + 3 ) * 2 ) * 3", ans: "24", equal: "=" };
+    this.state = { formula: "((1+3)*-2)*3", ans: "24", equal: "=" };
 
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ formula: event.target.value });
-    this.setState({ equal: event.target.value.length > 0 ? "=" : "" });
-    this.setState({ ans: wasm.calc(event.target.value) });
+    let formula = event.target.value;
+    this.setState({ formula: formula });
+    this.setState({ equal: formula.length ? "=" : "" });
+    this.setState({ ans: wasm.calc(formula) });
+  }
+
+  addChar(c: string) {
+    let formula = this.state.formula + c;
+    this.setState({ formula: formula });
+    this.setState({ ans: wasm.calc(formula) });
   }
 
   render() {
     return (
-      <div className="formula">
-        <input
-          type="text"
-          value={this.state.formula}
-          onChange={this.handleChange}
-        />
-        {this.state.equal} {this.state.ans}
+      <div className="calculator_frame">
+        <div className="formula">
+          <input
+            type="text"
+            value={this.state.formula}
+            onChange={this.handleChange}
+          />
+          {this.state.equal} {this.state.ans}
+        </div>
+        <table>
+          <tbody>
+            <tr>
+              {Object.keys([...Array(10)]).map((num) => (
+                <td>
+                  <button
+                    className="calc_button"
+                    onClick={() => this.addChar(`${num}`)}
+                  >
+                    {num}
+                  </button>
+                </td>
+              ))}
+            </tr>
+            <tr>
+              {["+", "-", "*", "/", "%", "(", ")"].map((op) => (
+                <td>
+                  <button
+                    className="calc_button"
+                    onClick={() => this.addChar(op)}
+                  >
+                    {op}
+                  </button>
+                </td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
@@ -46,7 +83,7 @@ const Calc = () => {
   return (
     <div>
       <h1>Calculator</h1>
-      <NameForm />
+      <CalcForm />
     </div>
   );
 };
