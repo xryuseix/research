3159d943a4da470f8aaf211d1fcd707dd3b2ed21
xryuseix <ryusei143.shootingstar@gmail.com>
2021-11-14 06:38:22 +0900

    fix: レスポンシブ対応をした

--- website/components/calculator.tsx
+++ website/components/calculator.tsx
@@ -17,7 +17,7 @@ interface State {
 class CalcForm extends React.Component<{}, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { formula: "((1+3)*-2)*3", ans: "24", equal: "=" };
+    this.state = { formula: "((1+3)*-2)*3", ans: "-24", equal: "=" };
 
     this.handleChange = this.handleChange.bind(this);
   }
@@ -35,6 +35,11 @@ class CalcForm extends React.Component<{}, State> {
     this.setState({ ans: wasm.calc(formula) });
   }
 
+  reCalc() {
+    let formula = this.state.formula;
+    this.setState({ ans: wasm.calc(formula) });
+  }
+
   render() {
     return (
       <div className={calcStyles.calculator_frame}>
@@ -43,37 +48,38 @@ class CalcForm extends React.Component<{}, State> {
             type="text"
             value={this.state.formula}
             onChange={this.handleChange}
+            className={calcStyles.input}
           />
           {this.state.equal} {this.state.ans}
         </div>
-        <table>
-          <tbody>
-            <tr className={calcStyles.calc_button_num}>
-              {Object.keys([...Array(10)]).map((num) => (
-                <td key={num}>
-                  <button
-                    className={calcStyles.calc_button}
-                    onClick={() => this.addChar(`${num}`)}
-                  >
-                    {num}
-                  </button>
-                </td>
-              ))}
-            </tr>
-            <tr className={calcStyles.calc_button_op}>
-              {["+", "-", "*", "/", "%", "(", ")"].map((op) => (
-                <td key={op}>
-                  <button
-                    className={calcStyles.calc_button}
-                    onClick={() => this.addChar(op)}
-                  >
-                    {op}
-                  </button>
-                </td>
-              ))}
-            </tr>
-          </tbody>
-        </table>
+        <div>
+          <div className={calcStyles.calc_button_num}>
+            {Object.keys([...Array(10)]).map((num) => (
+              <button
+                className={calcStyles.calc_button}
+                onClick={() => this.addChar(`${num}`)}
+              >
+                {num}
+              </button>
+            ))}
+          </div>
+          <div className={calcStyles.calc_button_op}>
+            {["+", "-", "*", "/", "%", "(", ")"].map((op) => (
+              <button
+                className={calcStyles.calc_button}
+                onClick={() => this.addChar(op)}
+              >
+                {op}
+              </button>
+            ))}
+            <button
+              className={calcStyles.calc_recalc_button}
+              onClick={() => this.reCalc()}
+            >
+              recalc
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
