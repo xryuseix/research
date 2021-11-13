0977545cd38d1af546ab5f19ea221f57bf9af8d9
xryuseix <ryusei143.shootingstar@gmail.com>
2021-11-14 06:09:55 +0900

    feat: add texk page

--- website/components/Layout/MenuBar.tsx
+++ website/components/Layout/MenuBar.tsx
@@ -76,8 +76,7 @@ const MenuBar: FC = ({ children }) => {
       </Box>
 
       <Drawer
-        // variant="temporary"
-        variant="permanent"
+        variant="temporary"
         open={drawerOpen}
         onClose={handleDrawerToggle}
       >
---
+++ website/pages/info/tech.tsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import { Layout } from "../../components/Layout";
+
+const Tech = () => {
+  return (
+    <Layout
+      title="このサイトを構成する技術"
+      description="このサイトの技術構成を紹介"
+    >
+      <h1>このサイトを構成する技術</h1>
+      <img src="/research/tech.png" width="100%" />
+    </Layout>
+  );
+};
+
+export default Tech;
