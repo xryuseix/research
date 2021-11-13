4802349b077e9d1d2a64ad43805db1d4fa9dff5f
xryuseix <ryusei143.shootingstar@gmail.com>
2021-11-13 17:21:18 +0900

    fix: wasmのビルドエラーの修正

--- website/next.config.js
+++ website/next.config.js
@@ -5,9 +5,20 @@
 
 module.exports = {
   reactStrictMode: true,
-  webpack(config) {
+  basePath: '/research',
+  webpack(config, { isServer, dev }) {
+    // Enable webassembly
     config.experiments = { asyncWebAssembly: true };
+
+    // In prod mode and in the server bundle (the place where this "chunks" bug
+    // appears), use the client static directory for the same .wasm bundle
+    config.output.webassemblyModuleFilename =
+      isServer && !dev ? "../static/wasm/[id].wasm" : "static/wasm/[id].wasm";
+
+    // Ensure the filename for the .wasm bundle is the same on both the client
+    // and the server (as in any other mode the ID's won't match)
+    config.optimization.moduleIds = "named";
+
     return config;
   },
-  basePath: '/research',
 };
