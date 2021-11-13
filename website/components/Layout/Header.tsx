b63e3544629c96c24b31d3f4a9151c39c28dae18
xryuseix <ryusei143.shootingstar@gmail.com>
2021-11-14 02:15:36 +0900

    fix: サイドバーのリンクの範囲を修正

--- website/components/Layout/Header.tsx
+++ website/components/Layout/Header.tsx
@@ -10,8 +10,9 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { listItemClasses } from "@mui/material/ListItem";
-import ListItem from "@mui/material/ListItem";
-import List from '@mui/material/List';
+import ListItem, { ListItemProps } from "@mui/material/ListItem";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
 
 import Button from "@material-ui/core/Button";
 import Drawer from "@material-ui/core/Drawer";
@@ -19,7 +20,7 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 
 import HomeIcon from "@material-ui/icons/Home";
-import CalculateIcon from '@mui/icons-material/Calculate';
+import CalculateIcon from "@mui/icons-material/Calculate";
 
 const titleStyle: { [key: string]: string | number } = {
   textTransform: "none",
@@ -27,6 +28,22 @@ const titleStyle: { [key: string]: string | number } = {
   fontSize: "1.2em",
   marginLeft: "1em",
 };
+
+type LinkListItemProps = Omit<
+  ListItemProps<"a", { href: string }>,
+  "component" | "button"
+>;
+
+const LinkListItem = React.forwardRef<HTMLAnchorElement, LinkListItemProps>(
+  function LinkListItem(props, forwardedRef) {
+    const { href, ...other } = props;
+    return (
+      <Link href={href}>
+        <ListItem component="a" button ref={forwardedRef} {...other} />
+      </Link>
+    );
+  }
+);
 
 export const Header: FC = ({ children }) => {
   const [drawerOpen, setDrawerOpen] = React.useState(false);
@@ -71,28 +88,21 @@ export const Header: FC = ({ children }) => {
                 fill: "#474b42",
               },
               backgroundColor: "#f5f5f5",
-            }
+            },
           }}
         >
-          <ListItem >
+          <LinkListItem href="/">
             <ListItemIcon>
               <HomeIcon />
             </ListItemIcon>
-            <Link href="/">
-              <ListItemText primary="Home" />
-            </Link>
-          </ListItem>
-          <ListItem>
+            <ListItemText primary="Home" />
+          </LinkListItem>
+          <LinkListItem href="/apps/calc">
             <ListItemIcon>
               <CalculateIcon />
             </ListItemIcon>
-            <Link href="/apps/calc">
-              <ListItemText
-                primary="Calculator"
-                secondary="Using WebAssembly"
-              />
-            </Link>
-          </ListItem>
+            <ListItemText primary="Calculator" secondary="Using WebAssembly" />
+          </LinkListItem>
         </List>
       </Drawer>
       <div>{children}</div>
