4b84ad83d2b9b81866853e47cee5bbfebd12975f
xryuseix <ryusei143.shootingstar@gmail.com>
2021-11-14 20:35:42 +0900

    fix: 電卓がスマホでいい感じに見えるようにした

--- website/components/calculator.tsx
+++ website/components/calculator.tsx
@@ -11,44 +11,46 @@ type Props = {
 interface State {
   formula: string;
   ans: string;
-  equal: string;
 }
 
 class CalcForm extends React.Component<{}, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { formula: "((1+3)*-2)*3", ans: "-24", equal: "=" };
-
+    this.state = { formula: "((1+3)*-2)*3", ans: "-24" };
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  reCalc(formula: string) {
+    if (formula.length > 0) {
+      let ans = wasm.calc(formula);
+      this.setState({ ans: ans });
+    } else {
+      this.setState({ ans: "0" });
+    }
   }
 
   handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     let formula = event.target.value;
     this.setState({ formula: formula });
-    this.setState({ equal: formula.length ? "=" : "" });
-    this.setState({ ans: wasm.calc(formula) });
+    this.reCalc(formula);
   }
 
   addChar(c: string) {
     let formula = this.state.formula + c;
     this.setState({ formula: formula });
-    this.setState({ ans: wasm.calc(formula) });
+    this.reCalc(formula);
   }
 
   deleteChar() {
     if (this.state.formula.length > 0) {
       let formula = this.state.formula.slice(0, -1);
       this.setState({ formula: formula });
-      this.setState({ ans: wasm.calc(formula) });
+      this.reCalc(formula);
     }
   }
 
-  reCalc() {
-    let formula = this.state.formula;
-    this.setState({ ans: wasm.calc(formula) });
-  }
-
   render() {
+
     return (
       <div className={calcStyles.calculator_frame}>
         <div className={calcStyles.formula}>
@@ -58,7 +60,7 @@ class CalcForm extends React.Component<{}, State> {
             onChange={this.handleChange}
             className={calcStyles.input}
           />
-          {this.state.equal} {this.state.ans}
+          <div className={calcStyles.formula_ans}>= {this.state.ans}</div>
         </div>
         <div>
           <div className={calcStyles.calc_button_num}>
@@ -87,12 +89,6 @@ class CalcForm extends React.Component<{}, State> {
               onClick={() => this.deleteChar()}
             >
               delete
-            </button>
-            <button
-              className={`${calcStyles.calc_button} ${calcStyles.red}`}
-              onClick={() => this.reCalc()}
-            >
-              recalc
             </button>
           </div>
         </div>
