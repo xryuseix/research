6b1c3044fc88480fb79ff60583603cf2ed438b92
xryuseix <ryusei143.shootingstar@gmail.com>
2021-11-21 17:54:52 +0900

    chore: update build command

--- website/develop.sh
+++ website/develop.sh
@@ -1,5 +1,7 @@
-cd ../problem/calculator
-wasm-pack build
-cd ../../website
-yarn install
+cd ../problem/
+cd calculator && wasm-pack build && cd ..
+cd make10puzzle && wasm-pack build && cd ..
+cd ..
+cd website && yarn install
 cp -r ../problem/calculator/pkg/ node_modules/calculator/
+cp -r ../problem/make10puzzle/pkg/ node_modules/make10puzzle/
